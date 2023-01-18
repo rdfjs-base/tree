5dbbcb59358aabff0420fc51c9360f296637b8c3
bergi <bergi@axolotlfarm.org>
2023-01-18 23:42:43 +0100

    initial commit

---
+++ test/Tree.test.js
@@ -0,0 +1,98 @@
+import { strictEqual } from 'assert'
+import TermMap from '@rdfjs/term-map'
+import { describe, it } from 'mocha'
+import Tree from '../lib/Tree.js'
+import { testTree } from './support/testTree.js'
+
+describe('Tree', () => {
+  it('should be a constructor', () => {
+    strictEqual(typeof Tree, 'function')
+  })
+
+  describe('.nodes', () => {
+    it('should be a TermMap property', () => {
+      const tree = new Tree([])
+
+      strictEqual(tree.nodes instanceof TermMap, true)
+    })
+  })
+
+  describe('.objects', () => {
+    it('should be a TermMap property', () => {
+      const tree = new Tree()
+
+      strictEqual(tree.objects instanceof TermMap, true)
+    })
+  })
+
+  describe('.subjects', () => {
+    it('should be a TermMap property', () => {
+      const tree = new Tree()
+
+      strictEqual(tree.subjects instanceof TermMap, true)
+    })
+  })
+
+  describe('', () => {
+    it('should parse lists', async () => {
+      const expected = {
+        objects: [{
+          term: ''
+        }, {
+          term: '"a"'
+        }, {
+          term: '"b"'
+        }, {
+          term: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#nil>'
+        }],
+        subjects: [{
+          items: '',
+          predicates: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#first> <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest>',
+          term: ''
+        }, {
+          items: '"a" "b"',
+          predicates: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#first> <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest>',
+          term: ''
+        }, {
+          items: '',
+          predicates: '<http://example.org/propertyC> <http://example.org/propertyD>',
+          term: '<http://example.org/resource>'
+        }]
+      }
+
+      await testTree('lists', expected)
+    })
+
+    it('should parse multiple objects', async () => {
+      const expected = {
+        objects: [{
+          term: '"a"'
+        }, {
+          term: '"b"'
+        }],
+        subjects: [{
+          items: '',
+          predicates: '<http://example.org/propertyA>',
+          term: '<http://example.org/resource>'
+        }]
+      }
+
+      await testTree('objects', expected)
+    })
+
+    it('should parse type', async () => {
+      const expected = {
+        objects: [{
+          term: '<http://example.org/Resource>'
+        }],
+        subjects: [{
+          items: '',
+          predicates: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>',
+          term: '<http://example.org/resource>'
+        }]
+      }
+
+      await testTree('type', expected)
+    })
+  })
+})
